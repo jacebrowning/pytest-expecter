72f040069f6982c13f00d7983055ebf7243bbd4d
Jace Browning <jacebrowning@gmail.com>
2017-12-02 12:32:54 -0500

    Bump version to 0.2.2.post7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="pytest-expecter",
-    version='0.2.2.post6',
+    version='0.2.2.post7',
     description="A fork of 'expecter' with better support for pytest.",
     long_description=README,
     author="Jace Browning",
